4dbbe64c639b862fa0ad766fcf499d323171a3bd
nmfirdausw <nmfirdausw@gmail.com>
2026-09-10 16:01:58 +0800

    Map q to close fyler and restore the previous buffer
    Workaround for fyler.nvim issue #366

--- lua/filemanager.lua
+++ lua/filemanager.lua
@@ -5,6 +5,10 @@ vim.pack.add({
 
 local fyler = require("fyler")
 
+-- WORKAROUND for https://github.com/FylerOrg/fyler.nvim/issues/366
+-- TODO: remove once #366 is fixed upstream.
+local pre_open_buf
+
 -- Set up fyler with default config
 fyler.setup({
   mappings = {
@@ -19,9 +23,26 @@ fyler.setup({
         args = { parent = true },
         desc = "Collapse parent directory",
       },
+      ["q"] = {
+        action = function(instance)
+          instance:close()
+
+          if
+            pre_open_buf
+            and vim.api.nvim_buf_is_valid(pre_open_buf)
+            and vim.api.nvim_get_current_buf() ~= pre_open_buf
+          then
+            vim.api.nvim_set_current_buf(pre_open_buf)
+          end
+        end,
+        desc = "Quit",
+      },
     },
   },
 })
 
 -- Open file manager
-vim.keymap.set("n", "<BS>", fyler.open, { desc = "File Manager - Fyler" })
+vim.keymap.set("n", "<BS>", function()
+  pre_open_buf = vim.api.nvim_get_current_buf()
+  fyler.open()
+end, { desc = "File Manager - Fyler" })
