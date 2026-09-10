1bd614c98a824ae115a67b65819ce026f7e7bad6
nmfirdausw <nmfirdausw@gmail.com>
2026-09-10 17:51:47 +0800

    Add keymaps to toggle inlay hints and codelens

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,3 +2,14 @@
 vim.lsp.enable({
   "lua_ls",
 })
+
+
+-- Toggle inlay hints
+vim.keymap.set("n", "<leader>th", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "Toggle inlay hints" })
+
+-- Toggle codelens
+vim.keymap.set("n", "<leader>tc", function()
+  vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled())
+end, { desc = "Toggle codelens" })
