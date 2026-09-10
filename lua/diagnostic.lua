910cc093486243ef1d40a542968f6cb5ce524df3
nmfirdausw <nmfirdausw@gmail.com>
2026-09-10 19:15:13 +0800

    Configure diagnostic display options

---
+++ lua/diagnostic.lua
@@ -0,0 +1,8 @@
+-- Diagnostic display settings
+vim.diagnostic.config({
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+  virtual_text = { current_line = true },
+  float = { border = "none" },
+})
