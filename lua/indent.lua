f5e7140c87123be628cceb5a5d3369930d4ddaef
nmfirdausw <nmfirdausw@gmail.com>
2026-09-10 18:03:20 +0800

    Add per-filetype indent overrides with 2 spaces for lua

--- lua/indent.lua
+++ lua/indent.lua
@@ -4,3 +4,22 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+-- Per-filetype indent overrides
+local indent_overrides = {
+  lua = { tabstop = 2, shiftwidth = 2, softtabstop = 2 },
+}
+
+-- Apply the overrides per buffer on FileType
+local group = vim.api.nvim_create_augroup("indent_overrides", { clear = true })
+for ft, opts in pairs(indent_overrides) do
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = ft,
+    callback = function()
+      for k, v in pairs(opts) do
+        vim.opt_local[k] = v
+      end
+    end,
+  })
+end
