69473337b4602e96fc7b08240e54c9e8c415ba7c
nmfirdausw <nmfirdausw@gmail.com>
2026-09-10 15:38:06 +0800

    Swap `Backspace` and `-` in fyler mappings

--- lua/filemanager.lua
+++ lua/filemanager.lua
@@ -6,7 +6,22 @@ vim.pack.add({
 local fyler = require("fyler")
 
 -- Set up fyler with default config
-fyler.setup({})
+fyler.setup({
+  mappings = {
+    n = {
+      ["<BS>"] = {
+        action = "visit",
+        args = { parent = true },
+        desc = "Go to parent directory",
+      },
+      ["-"] = {
+        action = "shrink",
+        args = { parent = true },
+        desc = "Collapse parent directory",
+      },
+    },
+  },
+})
 
 -- Open file manager
 vim.keymap.set("n", "<BS>", fyler.open, { desc = "File Manager - Fyler" })
